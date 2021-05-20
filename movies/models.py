9039682e3e6b098d4c29e7efaa322a17cf120033
JAESANG LEE <jake0403@naver.com>
2021-05-20 16:43:56 +0900

    feat: create movie app, fix movie model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Genre(models.Model):
+    genre_id = models.IntegerField()
+    genre_name = models.CharField(max_length=100)
+    # movies = models.ManyToManyField(Movie)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    released_date = models.TextField()
+    poster_path = models.CharField(max_length=300)
+    genres = models.ManyToManyField(Genre)
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'movies/index.html')
+
+
+def get_movies(request, pk):
+    pass
 
 def detail(request, pk):
     pass
