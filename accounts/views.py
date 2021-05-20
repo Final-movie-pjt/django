54d4fd6e2511758d7a4923c219ad97ba7f34c96b
Yongbbbba <lisagamer123@gmail.com>
2021-05-20 13:15:24 +0900

    feat: login, signup of accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,41 @@
 from django.http import request
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import CustomUserCreationForm
-from django.views.decorators.http import require_POST, require_GET
+from django.views.decorators.http import require_POST, require_GET, require_http_methods
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 # Create your views here.
-def signup(request):
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+    
     if request.method == 'POST':
         pass
+
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+    
+
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)        
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('movies:index')
     else:
         form = CustomUserCreationForm()
     context = {
--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    return render(request, 'movies/index.html')
 
 def detail(request, pk):
     pass
