d20127b3c3782b3d96926a2d0ce351d0644df656
JAESANG LEE <jake0403@naver.com>
2021-05-23 17:39:12 +0900

    feat: content-based 추천 기능 구현

--- movies/models.py
+++ movies/models.py
@@ -13,4 +13,7 @@ class Movie(models.Model):
     # released_date = models.TextField()
     released_date = models.DateField(auto_now=False, auto_now_add=False, null=True)
     poster_path = models.CharField(max_length=300, null=True)
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
     genres = models.ManyToManyField(Genre)
+    recommend_movies = models.ManyToManyField('self', symmetrical=False, related_name = 'recommended')
