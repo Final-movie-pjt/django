ec07d5cfe61646b2b839f307ca3a63407c358f8e
Yongbbbba <lisagamer123@gmail.com>
2021-05-26 14:05:13 +0900

    fix: asgi import bug

--- movieTalk/asgi.py
+++ movieTalk/asgi.py
@@ -8,16 +8,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movieTalk.settings')
+# django.setup()
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movieTalk.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
